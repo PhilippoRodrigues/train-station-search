6122ed7a94f4a508d3011ee38ead9d2accb4b2ba
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-11-08 21:15:49 -0300

    First commit

---
+++ src/main/java/com/philippo/trainstation/TrainStationSearchApplication.java
@@ -0,0 +1,14 @@
+package com.philippo.trainstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainStationSearchApplication
+{
+
+    public static void main ( String[] args ) {
+        SpringApplication.run(TrainStationSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/philippo/trainstation/TrainStationSearchApplicationTests.java
@@ -0,0 +1,14 @@
+package com.philippo.trainstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainStationSearchApplicationTests
+{
+
+    @Test
+    void contextLoads () {
+    }
+
+}
